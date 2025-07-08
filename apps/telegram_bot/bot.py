f99a805ec3d6ecd82b92bc95a417466867fc953e
Hosein Sarafrazi <sh.sarafrazi80@gmail.com>
2025-07-09 00:04:07 +0330

    clean code

--- apps/telegram_bot/bot.py
+++ apps/telegram_bot/bot.py
@@ -1,29 +1,33 @@
-import telebot
 import os
+
+import telebot
 from dotenv import load_dotenv
+from telebot import apihelper
 
 from config.settings import BASE_DIR
-from telebot import apihelper
 
 # Configure proxy
 apihelper.proxy = {
-    'http': 'http://localhost:2080',
-    'https': 'http://localhost:2080',
+    "http": "http://localhost:2080",
+    "https": "http://localhost:2080",
 }
 
-load_dotenv(BASE_DIR / '.env')
-API_TOKEN = os.environ.get('TELEGRAM_BOT_API_TOKEN',"")
+load_dotenv(BASE_DIR / ".env")
+API_TOKEN = os.environ.get("TELEGRAM_BOT_API_TOKEN", "")
 
 bot = telebot.TeleBot(API_TOKEN)
 
 
 # Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
+@bot.message_handler(commands=["help", "start"])
 def send_welcome(message):
-    bot.reply_to(message, """\
+    bot.reply_to(
+        message,
+        """\
 Hi there, I am EchoBot.
 I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
+""",
+    )
 
 
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
